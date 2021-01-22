95b18a274bd8d100c5e2fa8007c65a134a651960
bbbrito <32854220+bbbrito@users.noreply.github.com>
2021-01-21 23:41:14 -0300

    Ajustes no login

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ $(function() {
 				$("#btn_login").parent().siblings(".help-block").html(loadingImg("Verificando..."));
 			},
 			success: function(json) {
-				if (json["status"] == 1) {
+				if (json["status"]) {
 					clearErrors();
 					$("#btn_login").parent().siblings(".help-block").html(loadingImg("Logando..."));
 					window.location = BASE_URL + "restrict";
@@ -34,8 +34,13 @@ $(function() {
 		$("#modal_user").modal();
 	});
 
-	$("#form_new_user").submit(function() {
+	$("#forgot_password").click(function(){
+		clearErrors();
+		$("#form_forgot_password")[0].reset();
+		$("#modal_forgot_password").modal();
+	});
 
+	$("#form_new_user").submit(function() {
 		$.ajax({
 			type: "POST",
 			url: BASE_URL + "restrict/ajax_save_user",
@@ -48,15 +53,106 @@ $(function() {
 			success: function(response) {
 				clearErrors();
 				if (response["status"]) {
+				swal("Sucesso!","Usuário salvo com sucesso! Você receberá um e-mail com um link para confirmação de cadastro.", "success");
 					$("#modal_user").modal("hide");
-					swal("Sucesso!","Usuário salvo com sucesso!", "success");
+							$.ajax({
+								type: "POST",
+								data: "email="+response["confirmation"]["email_user"]+"&token="+response["confirmation"]["token"],
+								url: BASE_URL + "restrict/send_new_user_email",
+								success: function(response) {
+									clearErrors();
+								}
+							})
 				} else {
-					showErrorsModal(response["error_list"])
+					showErrorsModal(response["error_list"]);
 				}
 			}
 		})
 
 		return false;
 	});
+
+	$("#form_forgot_password").submit(function() {
+
+		$.ajax({
+			type: "POST",
+			url: BASE_URL + "restrict/ajax_forgot_password",
+			dataType: "json",
+			data: $(this).serialize(),
+			beforeSend: function() {
+				clearErrors();
+				$("#btn_forgot_password").siblings(".help-block").html(loadingImg("Verificando..."));
+			},
+			success: function(response) {
+				clearErrors();
+				if (response["status"]) {
+				swal("Verifique seu e-mail!","Siga as instruções no e-mail que enviamos a "+response["confirmation"]["email_user"]+" para cadastrar nova senha. Faça isso imediatamente, porque o link no e-mail expirará em breve.", "success");				
+					$("#modal_forgot_password").modal("hide");
+							$.ajax({
+								type: "POST",
+								data: "email="+response["confirmation"]["email_user"]+"&token="+response["confirmation"]["token"],
+								url: BASE_URL + "restrict/send_new_password_email",
+								success: function(response) {
+									clearErrors();
+								}
+							})
+				} else {
+					showErrorsModal(response["error_list"]);
+				}
+			}
+		})
+
+		return false;
+	});
+
+	$("#form_password").submit(function() {
+
+		$.ajax({
+			type: "POST",
+			url: BASE_URL + "password/ajax_save_new_password",
+			dataType: "json",
+			data: $(this).serialize(),
+			beforeSend: function() {
+				clearErrors();
+				$("#btn_new_password").siblings(".help-block").html(loadingImg("Verificando..."));
+			},
+			success: function(response) {
+				clearErrors();
+				if (response["status"] == 1) {
+					swal({
+						title: "Sucesso!",
+						text: "Nova senha cadastrada com sucesso",
+						type: "success",
+						confirmButtonText: "Ok",
+						closeOnConfirm: true,
+					}).then((result) => {
+						if (result.value) {
+							window.location = BASE_URL + "restrict";
+						}
+					})
+				} 
+				else if (response["status"] == 2) {
+					swal({
+						title: "Atenção!",
+						text: "Este link já expirou!",
+						type: "warning",
+						confirmButtonColor: "#d9534f",
+						confirmButtonText: "Ok",
+						closeOnConfirm: true,
+					}).then((result) => {
+						if (result.value) {
+							window.location = BASE_URL + "restrict";
+						}
+					})
+				}
+				else {
+					showErrors(response["error_list"]);
+				}
+			}
+		})
+
+		return false;
+	});
+
 
 })
