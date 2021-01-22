80ae69b53f31ca9d239b3e091d46f4a11a6edcc0
bbbrito <32854220+bbbrito@users.noreply.github.com>
2021-01-21 23:43:41 -0300

    Ajustes no util

--- public/js/util.js
+++ public/js/util.js
@@ -1,5 +1,8 @@
 const BASE_URL = "http://localhost/freteaqui/";
 
+const SITE_KEY = "6Ldimc0ZAAAAAG8IIhHs1zFlz08sTIJVOAf2cWIZ";
+
+
 const DATATABLE_PTBR = {
     "sEmptyTable": "Nenhum registro encontrado",
     "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
@@ -43,23 +46,25 @@ function showErrorsModal(error_list) {
 
 	$.each(error_list, function(id, message) {
 		$(id).parent().parent().addClass("has-error");
-		$(id).siblings(".help-block").html(message)
+		$(id).siblings(".help-block").html(message);
 	})
-} 
+}
 
 function loadingImg(message=""){
 	return "<i class='icon-circle-o-notch'></i>&nbsp;" + message;
 }
 
-function uploadImg(input_file, img, input_path){
+/*function uploadImg2(input_file, img, input_path){
 	
 	src_before = img.attr("src");
 	img_file = input_file[0].files[0];
 	form_data = new FormData();
 
 	form_data.append("image_file", img_file);
+	/*console.log(img_file);
+	console.log(form_data);*/
 
-	$.ajax({
+	/*$.ajax({
 		url: BASE_URL + "restrict/ajax_import_image",
 		dataType: "json",
 		cache: false,
@@ -76,6 +81,7 @@ function uploadImg(input_file, img, input_path){
 			if(response["status"]) {
 				img.attr("src", response["img_path"]);
 				input_path.val(response["img_path"]);
+
 			} else {
 				img.attr("src", src_before);
 				input_path.siblings(".help-block").html(response["error"]);
@@ -85,4 +91,97 @@ function uploadImg(input_file, img, input_path){
 			img.attr("src", src_before);
 		}
 	})
+}*/
+
+function uploadImg(input_file, img_view, input_path){
+	
+	src_before = img_view.attr("src");
+
+	var files = input_file[0].files;
+	form_data = new FormData();
+
+	for (var count = 0; count < files.length; count++) {
+			form_data.append("files[]", files[count]);
+		}
+
+	$.ajax({
+		url: BASE_URL + "restrict/ajax_import_image",
+		dataType: "json",
+		type: "POST",
+		data: form_data,
+		contentType: false,
+		cache: false,
+		processData: false,	
+		beforeSend: function() {
+			clearErrors();
+			input_path.siblings(".help-block").html(loadingImg("Carregando imagem..."));
+		},
+		success: function(response) {
+			clearErrors();
+			if(response["status"]) {
+				img_view.html(response["img_view"]);
+				input_path.val(response["img_path"]);
+			} else {
+				img_view.attr("src", src_before);
+				input_path.siblings(".help-block").html(response["error"]);
+			}
+		},
+		error: function() {
+			img_view.attr("src", src_before);
+		}
+	})
+}
+
+function uploadImg3(input_file, img, input_path){
+	
+	src_before = img.attr("src");
+
+	var img_view = [];
+	for (var i = 0; i < input_file[0].files.length; i++) {
+		img_view[i] = img;
+	}
+
+	var img_path = {};
+	for (var i = 0; i < input_file[0].files.length; i++) {
+		img_path[i] = input_path;
+	}
+
+	form_data = new FormData();
+
+	//form_data.append("image_file", img_file);
+
+	for (var i = 0; i < input_file[0].files.length; i++) {
+		form_data.append("image_file", input_file[0].files[i]);
+
+		$.ajax({
+			url: BASE_URL + "restrict/ajax_import_image",
+			dataType: "json",
+			cache: false,
+			contentType: false,
+			processData: false,
+			data: form_data,
+			type: "POST",
+			beforeSend: function() {
+				clearErrors();
+				input_path.siblings(".help-block").html(loadingImg("Carregando imagem..."));
+			},
+			success: function(response) {
+				clearErrors();
+				if(response["status"]) {
+				
+					img.attr("src", response["img_path"]);
+					input_path.val(response["img_path"]);
+
+				} else {
+					img.attr("src", src_before);
+					input_path.siblings(".help-block").html(response["error"]);
+				}
+			},
+			error: function() {
+				img.attr("src", src_before);
+			}
+		})
+	}
+
+	
 }
