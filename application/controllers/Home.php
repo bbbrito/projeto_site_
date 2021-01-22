1e0f22c2fa802241865882cfd04e4db9fe8c6574
bbbrito <32854220+bbbrito@users.noreply.github.com>
2021-01-21 23:44:53 -0300

    Ajustes na Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,19 +3,117 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index($id_city = NULL)
-	{
+	function __construct() {
+		parent::__construct();
+		$this->load->library("session");
+		$this->load->helper('url');
+		$this->load->library('user_agent');
+		$this->load->library("ajax_pagination");
 		$this->load->model("adverts_model");
+		$this->per_page = 12;
+	}
 
+	public function index()	{
+		//Get record count
+		if ($this->agent->browser() == 'Internet Explorer' and $this->agent->version() <= 11) {
+			$this->template->show("posts/unsupported_browser");
+		}
+
+		else {
+			$conditions["returnType"] = 'count'; 
+	        $totalRec = $this->adverts_model->getRows($conditions);
+
+			//Pagination configuration
+			$config = array (
+						'target' => "#advertList",
+						'base_url' => base_url("posts/ajax_search_data"),//rota que deve ser utilizada nos links de paginação
+						'total_rows' => $totalRec,
+						'per_page' => $this->per_page,//Identifica o número de registro a ser exibido por página
+						'link_func' => "searchFilter",
+						
+						//"num_links" => 3, //Número de links que devem ser exibidos (1 a 10, 2 a 12 etc)
+						//"uri_segment" => 3,//Identifica o segmento da url que vai determinar o número da página onde os registros devem ser recuperados e exibidos. No base_url, adverts é o 1, p é o 2, e o uri é o 3.
+						"full_tag_open" => "<ul class='pagination'>",
+						"full_tag_close" => "</ul>",
+						"first_link" => "Primeira",
+						"last_link" => FALSE,
+						"first_tag_open" => "<li>",
+						"first_tag_close" => "</li>",
+						"prev_link" => "Anterior",
+						"prev_tag_open" => "<li class='prev'>",
+						"prev_tag_close" => "</li>",
+						"next_link" => "Próxima",
+						"next_tag_open" => "<li class='next'>",
+						"next_tag_close" => "</li>",
+						"last_tag_open" => "<li>",
+						"last_tag_close" => "</li>",
+						"cur_tag_open" => "<li class='active'><a href='#'>",
+						"cur_tag_close" => "</a></li>",
+						"num_tag_open" => "<li>",
+						"num_tag_close" => "</li>",
+						"show_count" => FALSE
+					);
+			//Initialize pagination library
+			$this->ajax_pagination->initialize($config);
+			//Get records
+			$conditions = array('limit' => $this->per_page);
+			$data = array(
+					"styles" => array(
+						"bootstrap.css",
+						//"wing.min.css",
+					),
+					"scripts" => array(
+						"jquery.min.js",
+						//"bootstrap.min-3.4.1",
+						//"jquery-3.5.1.js",
+						//"jquery.easing.1.3.js",
+						//"bootstrap.min.js",
+						"jquery.waypoints.min.js",
+						"owl.carousel.min.js",
+						"main.js",
+						"util.js",
+						"home.js"
+					),
+					//"pagination" => $this->ajax_pagination->create_links(),//retorna o html completo de paginação
+					'adverts' => $this->adverts_model->getRows($conditions)
+				);
+			$this->template->show("posts/home", $data);
+		}
+	}
+
+	function ajax_search_data() {
+		//Define offset
+		$page = $this->input->post("page");
+		if(!$page) {
+			$offset = 0;
+		} else {
+			$offset = $page;
+		}
+		//Set conditions for search and filter
+		$keywords = $this->input->post("keywords");
+		$sortBy = $this->input->post("sortBy");
+		if (!empty($keywords)) {
+			$conditions["search"]["keywords"] = $keywords;
+		}
+		if (!empty($sortBy)) {
+			$conditions["search"]["sortBy"] = $sortBy;
+		}
+		//Get record count
+		$conditions["returnType"] = "count";
+		$totalRec = $this->adverts_model->getRows($conditions);
+		//Pagination configuration
 		$config = array (
-					"base_url" => base_url("adverts/p"),//rota que deve ser utilizada nos links de paginação
-					"per_page" => 2,//Identifica o número de registro a ser exibido por página
-					"num_links" => 3, //Número de links que devem ser exibidos (1 a 10, 2 a 12 etc)
-					"uri_segment" => 3,//Identifica o segmento da url que vai determinar o número da página onde os registros devem ser recuperados e exibidos. No base_url, adverts é o 1, p é o 2, e o uri é o 3.
-					"total_rows" => $this->adverts_model->countAll($id_city),
+					'target' => "#advertList",
+					'base_url' => base_url("posts/ajax_search_data"),//rota que deve ser utilizada nos links de paginação
+					'total_rows' => $totalRec,
+					'per_page' => $this->per_page,//Identifica o número de registro a ser exibido por página
+					'link_func' => "searchFilter",				
+					//"num_links" => 3, //Número de links que devem ser exibidos (1 a 10, 2 a 12 etc)
+					//"uri_segment" => 3,//Identifica o segmento da url que vai determinar o número da página onde os registros devem ser recuperados e exibidos. No base_url, adverts é o 1, p é o 2, e o uri é o 3.
+					//"total_rows" => $totalRec,
 					"full_tag_open" => "<ul class='pagination'>",
 					"full_tag_close" => "</ul>",
-					"first_link" => FALSE,
+					"first_link" => "Primeira",
 					"last_link" => FALSE,
 					"first_tag_open" => "<li>",
 					"first_tag_close" => "</li>",
@@ -30,11 +128,15 @@ class Home extends CI_Controller {
 					"cur_tag_open" => "<li class='active'><a href='#'>",
 					"cur_tag_close" => "</a></li>",
 					"num_tag_open" => "<li>",
-					"num_tag_close" => "</li>"
+					"num_tag_close" => "</li>",
+					"show_count" => FALSE
 				);
-		$this->pagination->initialize($config);
-		$offset = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-		$adverts = $this->adverts_model->show_adverts("id_advert", "desc", $config["per_page"], $offset, $id_city);
+		//Initialize pagination library
+		$this->ajax_pagination->initialize($config);
+		//Get records
+		$conditions['start'] = $offset; 
+        $conditions['limit'] = $this->per_page; 
+        unset($conditions['returnType']); 
 		$data = array(
 				"styles" => array(
 					"bootstrap.css",
@@ -52,136 +154,13 @@ class Home extends CI_Controller {
 					"util.js",
 					"home.js"
 				),
-				"pagination" => $this->pagination->create_links(),//retorna o html completo de paginação
-				"adverts" => $adverts
+				//"pagination" => $this->ajax_pagination->create_links(),//retorna o html completo de paginação
+				"adverts" => $this->adverts_model->getRows($conditions)
 			);
-			$this->template->show("home.php", $data, false);
-	}
-
-
-	/*private function a()
-    {
-    	$variavel = [];
-    	$variavel[0] = $this->ajax_adverts_filter_show();
-
-        return $variavel[0];
-    }*/
-	//Filtro por estado
-	/*public function ajax_adverts_filter_show() {
-
-		if (!$this->input->is_ajax_request()) {
-			exit("Nenhum acesso de script direto permitido!");
-		}
-
-		$json = array();
-		$json["status"] = 1;
-	
-		$id_state = $this->input->post("id_state");
-		/*$this->load->library('../Home/Home_Filter_State');
-		$this->Home_Filter_State->index($id_state);*/
-		//$this->parser->parse('ajax/'+base_url(), $id_state);
-		
-		/*$json["state"] = $id_state;
-
-		echo json_encode($json);
-	}*/
-
-	/*function _remap($method, $args){
-
-	    if($method == "ajax_adverts_filter_show"){
-	      $this->index($args);
-	      //header("Refresh: 20 url=home/index");
-	  	}
-	    else
-	      $this->index();
-  }
-
-  /*function _remap($method, $args) {
-
-       if (method_exists($this, $method))
-       {
-            $this->Index($args);
-       }
-       else
-       {
-            $this->Index();
-       }
-	}*/
-
-
-	public function ajax_filtered_adverts_city($id_city = NULL)
-	{
-		if (!$this->input->is_ajax_request()) {
-			exit("Nenhum acesso de script direto permitido!");
-		}
-
-		$json = array();
-
-		$this->load->model("adverts_model");
-		$id_city = $this->input->post("id_city");
-
-		$config = array (
-					"base_url" => base_url("adverts/p"),//rota que deve ser utilizada nos links de paginação
-					"per_page" => 2,//Identifica o número de registro a ser exibido por página
-					"num_links" => 3, //Número de links que devem ser exibidos (1 a 10, 2 a 12 etc)
-					"uri_segment" => 3,//Identifica o segmento da url que vai determinar o número da página onde os registros devem ser recuperados e exibidos. No base_url, adverts é o 1, p é o 2, e o uri é o 3.
-					"total_rows" => $this->adverts_model->countAll($id_city),
-					"full_tag_open" => "<ul class='pagination'>",
-					"full_tag_close" => "</ul>",
-					"first_link" => FALSE,
-					"last_link" => FALSE,
-					"first_tag_open" => "<li>",
-					"first_tag_close" => "</li>",
-					"prev_link" => "Anterior",
-					"prev_tag_open" => "<li class='prev'>",
-					"prev_tag_close" => "</li>",
-					"next_link" => "Próxima",
-					"next_tag_open" => "<li class='next'>",
-					"next_tag_close" => "</li>",
-					"last_tag_open" => "<li>",
-					"last_tag_close" => "</li>",
-					"cur_tag_open" => "<li class='active'><a href='#'>",
-					"cur_tag_close" => "</a></li>",
-					"num_tag_open" => "<li>",
-					"num_tag_close" => "</li>"
-				);
-		$this->pagination->initialize($config);
-		$offset = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-		$filtered_adverts_city = $this->adverts_model->show_adverts("id_advert", "desc", $config["per_page"], $offset, $id_city);
-		$data = array(
-				"styles" => array(
-					"bootstrap.css",
-					//"wing.min.css",
-				),
-				"scripts" => array(
-					"jquery.min.js",
-					//"bootstrap.min-3.4.1",
-					//"jquery-3.5.1.js",
-					//"jquery.easing.1.3.js",
-					//"bootstrap.min.js",
-					"jquery.waypoints.min.js",
-					"owl.carousel.min.js",
-					"main.js",
-					"util.js",
-					"home.js"
-				),
-				"pagination" => $this->pagination->create_links(),//retorna o html completo de paginação
-				"filtered_adverts_city" => $filtered_adverts_city
-			);
-			$this->load->view("home.php", $data, false);
-			$this->template->show("home.php", $data, false);
-
-
-		//$json["filtered_adverts_city"] = $filtered_adverts_city;
-		/*$json["pagination"] = $data["pagination"];*/
-		
-		//return ($filtered_adverts_city);
-		//echo $adverts;
-		//echo json_encode($filtered_adverts_city);
-		//return ("filtered_adverts_city");
+		//unset($conditions["start"]);
+		$this->template->show("posts/home_search.php", $data, false);
 	}
 		
-
 	//Adiciona número de visualizações do anúncio
 	public function ajax_views_count() {
 
@@ -194,7 +173,14 @@ class Home extends CI_Controller {
 
 		$this->load->model("adverts_model");
 		$id_advert = $this->input->post("id_advert");
-		$this->adverts_model->update_advert_count($id_advert);
+		if ($this->session->userdata("id_user")!== null) {
+			$id_user = $this->session->userdata("id_user");
+			if($this->adverts_model->is_duplicated("id_user_fk", $id_user, $id_advert)){
+				$this->adverts_model->update_advert_count($id_advert);
+			}
+		} else {
+			$this->adverts_model->update_advert_count($id_advert);
+		}
 
 		echo json_encode($json);
 	}
@@ -222,9 +208,120 @@ class Home extends CI_Controller {
 			$json["img"]["advert_data_slide_to"] .= '<li data-target="#faqui-carousel-'.$id_advert.'" data-slide-to="'. $i . '"></li>';
 
 			$json["img"]["advert_img_path_item"] .= '<div class="item"><img src="'.base_url() . $advert_img_path[$i] . '" class="img-advert-modal center-block" alt="Foto do veículo" >   </div>';
-        }     
+        }   
 		echo json_encode($json);
 	}
 
+	//Função criada para mostrar lista horizontal de cidades
+	function ajax_list_city_data() {
+		
+		$this->load->model("adverts_model");
+		$state = $this->input->post("id_state");
 
+		$data = $this->adverts_model->get_cities($state)->result_array();
+		$html = '';
+
+		if($data == NULL){
+			$html .= '<li class="slider-item item-visible"><a href="'.base_url(). 'restrict'.'" class="slab2-text"> Nenhum anúncio cadastrado neste Estado. Clique aqui e seja o primeiro a anunciar! </a></li>';
+		} else {
+			for ($i=0; $i < count($data); $i++) { 
+				$html .= '<li id="'.$data[$i]['city'].'" class="slider-item"><a href="#" class="slab2-text" title="'.$data[$i]['city'].'" style="display: block;" > '.$data[$i]['city'].' </a></li>';				
+        	}
+		}
+		echo $html;
+	}
+
+	//Filtro por cidade e categoria
+	function ajax_filter_city_data() {
+		//Define offset
+		$page = $this->input->post("page");
+		if(!$page) {
+			$offset = 0;
+		} else {
+			$offset = $page;
+		}
+		//Set conditions for search and filter
+		$city = $this->input->post("id_city");
+		$category = $this->input->post("category");
+
+		if (!empty($city)) {
+			$conditions["where"]["city"] = $city;
+		}
+		if (!empty($category)) {
+			$conditions["where"]["category"] = $category;
+		}
+		//Set conditions for search and filter
+		$sortBy = $this->input->post("sortBy");
+		if (!empty($sortBy)) {
+			$conditions["search"]["sortBy"] = $sortBy;
+		}
+
+		//Get record count
+		$conditions["returnType"] = "count";
+		$totalRec = $this->adverts_model->getRows($conditions);
+
+		//Pagination configuration
+		$config = array (
+					'target' => "#advertList",
+					'base_url' => base_url("posts/ajax_filter_city_data"),//rota que deve ser utilizada nos links de paginação
+					'total_rows' => $totalRec,
+					'per_page' => $this->per_page,//Identifica o número de registro a ser exibido por página
+					'link_func' => "cityFilter",
+						
+					//"num_links" => 3, //Número de links que devem ser exibidos (1 a 10, 2 a 12 etc)
+					//"uri_segment" => 3,//Identifica o segmento da url que vai determinar o número da página onde os registros devem ser recuperados e exibidos. No base_url, adverts é o 1, p é o 2, e o uri é o 3.
+					//"total_rows" => $totalRec,
+					"full_tag_open" => "<ul class='pagination'>",
+					"full_tag_close" => "</ul>",
+					"first_link" => "Primeira",
+					"last_link" => FALSE,
+					"first_tag_open" => "<li>",
+					"first_tag_close" => "</li>",
+					"prev_link" => "Anterior",
+					"prev_tag_open" => "<li class='prev'>",
+					"prev_tag_close" => "</li>",
+					"next_link" => "Próxima",
+					"next_tag_open" => "<li class='next'>",
+					"next_tag_close" => "</li>",
+					"last_tag_open" => "<li>",
+					"last_tag_close" => "</li>",
+					"cur_tag_open" => "<li class='active'><a href='#'>",
+					"cur_tag_close" => "</a></li>",
+					"num_tag_open" => "<li>",
+					"num_tag_close" => "</li>",
+					"show_count" => FALSE
+		);
+		//Initialize pagination library
+		$this->ajax_pagination->initialize($config);
+
+		//Get records
+		$conditions['start'] = $offset; 
+        $conditions['limit'] = $this->per_page; 
+        unset($conditions['returnType']); 
+
+		$data = array(
+				"styles" => array(
+					"bootstrap.css",
+					//"wing.min.css",
+				),
+				"scripts" => array(
+					"jquery.min.js",
+					//"bootstrap.min-3.4.1",
+					//"jquery-3.5.1.js",
+					//"jquery.easing.1.3.js",
+					//"bootstrap.min.js",
+					"jquery.waypoints.min.js",
+					"owl.carousel.min.js",
+					"main.js",
+					"util.js",
+					"home.js"
+				),
+				//"pagination" => $this->ajax_pagination->create_links(),//retorna o html completo de paginação
+				"adverts" => $this->adverts_model->getRows($conditions)
+		);
+		//unset($conditions["start"]);
+		$this->template->show("posts/home_filter_city.php", $data, false);
+	}
+
+	
 }
