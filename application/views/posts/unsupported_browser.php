4c0b15152bd3387f2fc3d660d1c0bd29ec35ae57
bbbrito <32854220+bbbrito@users.noreply.github.com>
2021-01-22 00:04:52 -0300

    Criação da view unsupported_browser

---
+++ application/views/posts/unsupported_browser.php
@@ -0,0 +1,36 @@
+<body>
+		<div class="incompatible-browser">
+    <div class="dialog">
+      <div class="content">
+        <div class="body">
+          <div class="message" align="center">
+            <h3>Você tem um navegador não suportado.</h3>
+            <p>Para obter a melhor experiência possível usando nosso site, recomendamos que você utilize outro navegador da web.</p>
+            <p>Uma lista dos navegadores mais populares pode ser encontrada abaixo. Basta clicar nos ícones para acessar a página de download.</p>
+            <ul class="list-unstyled browsers-list">
+              <li>
+                <a href="http://www.google.com/chrome" target="_blank">
+                  <img width="70" src="<?php echo base_url(); ?>public/images/chrome-512.png" border="0"><br>
+                  <span>Chrome</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://getfirefox.com/" target="_blank">
+                  <img width="70" src="<?php echo base_url(); ?>public/images/firefox-512.png" border="0"><br>
+                  <span>Firefox</span>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.apple.com/safari/download/" target="_blank">
+                  <img width="70" src="<?php echo base_url(); ?>public/images/safari-512.png" border="0"><br>
+                  <span>Safari</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+</body>
